5015ffbb70b6c0210b999cc252d1e805a0286510
Liesy <preposterous@163.com>
2022-06-03 17:03:33 +0800

    chore: 2022.6.3 exp5 func demo.

--- exp5/main_exp5.py
+++ exp5/main_exp5.py
@@ -1,13 +1,13 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
-import cv2 as cv
-import numpy as np
+import logging
 from argparse import ArgumentParser
-from os import path
 from typing import Tuple
+
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+from hmax import HMax
 
 seed = 42
 np.random.seed(seed)
@@ -18,7 +18,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def load_data() -> Tuple[DataLoader, DataLoader]:
     im_transform = transforms.Compose([
-        transforms.Totensor(),
+        transforms.ToTensor(),
         transforms.Normalize((0.1307), (0.3081))
     ])
     # Dataset
@@ -40,14 +40,25 @@ def load_data() -> Tuple[DataLoader, DataLoader]:
 
 
 def main():
-    ...
+    train_dataloader, test_dataloader = load_data()  # img:(64,1,28,28)  label:(64)
+    model = HMax()
+
+    it = iter(train_dataloader)
+    sample, label = it.__next__()
+
+    logging.info(f"the sample's shape is {sample.shape}")
+
+    output = model(sample)
+    logging.info(f"the output's shape is {output.shape}")
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('-data_path', type=str, default='./')
+    parser.add_argument('-data_path', type=str, default='../')
     parser.add_argument('-batch_size', type=int, default=64)
     parser.add_argument('-epoch', type=int, default=1000)
     args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(asctime)s - %(message)s')
 
     main()
